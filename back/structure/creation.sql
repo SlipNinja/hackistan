39cd7dbd2f72b7e124d797b21bd8d0183c07b369
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2026-02-10 09:41:25 +0100

    doc: add db data structure schema and creation requests

---
+++ back/structure/creation.sql
@@ -0,0 +1,42 @@
+CREATE TABLE users(
+   id_user INT AUTO_INCREMENT,
+   username VARCHAR(50) NOT NULL,
+   email VARCHAR(50) NOT NULL,
+   password VARCHAR(250) NOT NULL,
+   role VARCHAR(50) NOT NULL,
+   is_banned BOOLEAN DEFAULT false,
+   PRIMARY KEY(id_user),
+   UNIQUE(username),
+   UNIQUE(email)
+);
+
+CREATE TABLE discussions(
+   id_discussion INT AUTO_INCREMENT,
+   title VARCHAR(250) NOT NULL,
+   last_modified VARCHAR(50) NOT NULL,
+   PRIMARY KEY(id_discussion),
+   UNIQUE(title)
+);
+
+CREATE TABLE posts(
+   id_post INT AUTO_INCREMENT,
+   content TEXT NOT NULL,
+   date_posted VARCHAR(50) NOT NULL,
+   status ENUM("accepted", "pending", "banned") NOT NULL,
+   id_anwsered_post INT,
+   id_discussion INT NOT NULL,
+   id_user INT NOT NULL,
+   PRIMARY KEY(id_post),
+   FOREIGN KEY(id_anwsered_post) REFERENCES posts(id_post),
+   FOREIGN KEY(id_discussion) REFERENCES discussions(id_discussion),
+   FOREIGN KEY(id_user) REFERENCES users(id_user)
+);
+
+CREATE TABLE tags(
+   id_tag INT AUTO_INCREMENT,
+   name VARCHAR(50) NOT NULL,
+   id_discussion INT NOT NULL,
+   PRIMARY KEY(id_tag),
+   UNIQUE(name),
+   FOREIGN KEY(id_discussion) REFERENCES discussions(id_discussion)
+);
