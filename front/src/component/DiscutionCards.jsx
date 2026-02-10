6377049a8f74255450e01fc6ae6dd9283f113398
Doriane <doriane_87@hotmail.fr>
2026-02-10 16:44:38 +0100

    config axios

--- front/src/component/DiscutionCards.jsx
+++ front/src/component/DiscutionCards.jsx
@@ -1,42 +1,32 @@
 import "./../styles/component/discutionCards.css";
+import { FaMessage } from "react-icons/fa6";
+import api from './../api/axios';
+import { useState, useEffect } from "react";
 
 export default function discutionCards() {
-  const discutions = [
-      {
-    id: 1,
-    title: "Problème avec React",
-    description: "Je n’arrive pas à comprendre pourquoi mon composant ne se rend pas correctement.",
-    id_user: 1,
-    last_modified: "2026-02-08T14:32:00"
-  },
-  {
-    id: 2,
-    title: "CSS Flexbox ou Grid ?",
-    description: "Dans quels cas vaut-il mieux utiliser Flexbox plutôt que Grid ?",
-    id_user: 2,
-    last_modified: "2026-02-07T18:10:00"
-  },
-  {
-    id: 3,
-    title: "Vite ne démarre pas",
-    description: "Erreur au lancement du serveur Vite après l’installation d’un package.",
-    id_user: 3,
-    last_modified: "2026-02-06T09:45:00"
-  },
-  {
-    id: 4,
-    title: "Problème de sticky en CSS",
-    description: "Mon aside en position sticky ne reste pas fixé au scroll.",
-    id_user: 4,
-    last_modified: "2026-02-05T21:02:00"
+    const [discutions, setDiscutions] = useState([]);
+  
+    const users = [
+      { id: 1, username: "Alice" },
+      { id: 2, username: "Bob" },
+      { id: 3, username: "Charlie" },
+      { id: 4, username: "Diane" }
+    ];
+    const post={
+       post_count:1
+    }
+  useEffect(() => {
+async function getDiscution(){
+  try{
+    const data= await api.get("/discussions")
+    setDiscutions(data)
+    
+  } catch(err){
+    console.log(err)
   }
-];
-const users = [
-  { id: 1, username: "Alice" },
-  { id: 2, username: "Bob" },
-  { id: 3, username: "Charlie" },
-  { id: 4, username: "Diane" }
-];
+}
+getDiscution()
+  },[])
 
   return (
     <div className="discutionContainer">
@@ -50,10 +40,11 @@ const users = [
             <h3>{discussion.title}</h3>
             <p>{discussion.description}</p>
 
-            <div className="discutionMeta">
+            <div className="discutionLoop">
               <span>{user?.username}</span>
               <span>•</span>
               <span>{discussion.last_modified}</span>
+              <span className="postCount"><FaMessage /> {post?.post_count}</span>
             </div>
           </div>
         );
