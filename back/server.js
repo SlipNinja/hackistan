9601d9300729b48dbdf7bcbffc67ad0819f91cf9
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2026-02-11 10:09:51 +0100

    feat: add posts routes

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import user_router from "./routes/user_routes.js";
 import discution_router from "./routes/discution_routes.js";
+import post_router from "./routes/post_routes.js";
 import cors from "cors";
 import "dotenv/config";
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/users", user_router);
 app.use("/discutions", discution_router);
+app.use("/posts", post_router);
 
 const PORT = 3000;
 app.listen(PORT, () => {
