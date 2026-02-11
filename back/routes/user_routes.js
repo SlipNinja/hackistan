6e10bde65f1cbec7c81ff4d7bfbea998ea2a9b2d
SlipNinja <26084911+SlipNinja@users.noreply.github.com>
2026-02-11 10:20:31 +0100

    feat: add user routes

--- back/routes/user_routes.js
+++ back/routes/user_routes.js
@@ -4,5 +4,7 @@ import * as user_ctrl from "../controllers/user_controller.js";
 const user_router = express.Router();
 
 user_router.get("/", user_ctrl.getUsers);
+user_router.get("/:id", user_ctrl.getUserById);
+user_router.post("/", user_ctrl.createUser);
 
 export default user_router;
