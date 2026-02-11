4dbf1abd7974519d02ab3ea8228018871a0beb87
David Bairet <davidbairet@gmail.com>
2026-02-11 12:03:32 +0100

    Fix discussion form css import

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -1,9 +1,9 @@
 
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header"
 import Home from "./pages/home"
 import Footer from "./components/Footer"
-import "./styles/component/discussionForm.css";
+import "./styles/component/discutionForm.css";
 import "./styles/component/loginForm.css";
 
 import LoginForm from "./components/forms/LoginForm";
@@ -15,20 +15,20 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-        <Route path="/" element= {<Home/>}/>
-        <Route path="/discution/:id" element={<DiscutionPage />} />
-        <Route
-          path="/login"
-          element={<LoginForm onSubmit={(d) => console.log("login", d)} />}
-        />
-        <Route
-          path="/register"
-          element={<RegisterForm onSubmit={(d) => console.log("register", d)} />}
-        />
-        <Route
-          path="/discutionForm"
-          element={<DiscutionForm onSubmit={(d) => console.log("discution", d)} />}
-        />
+      <Route path="/" element={<Home />} />
+      <Route path="/discution/:id" element={<DiscutionPage />} />
+      <Route
+        path="/login"
+        element={<LoginForm onSubmit={(d) => console.log("login", d)} />}
+      />
+      <Route
+        path="/register"
+        element={<RegisterForm onSubmit={(d) => console.log("register", d)} />}
+      />
+      <Route
+        path="/discutionForm"
+        element={<DiscutionForm onSubmit={(d) => console.log("discution", d)} />}
+      />
       <Footer />
     </BrowserRouter>
   );
