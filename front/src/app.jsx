d826882a396ac3a6e7f937494b55173fe5c4bf51
David Bairet <davidbairet@gmail.com>
2026-02-11 14:52:21 +0100

    mise en css diff pages

--- front/src/app.jsx
+++ front/src/app.jsx
@@ -1,10 +1,12 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header"
-import Home from "./pages/home"
-import Footer from "./components/Footer"
+import Header from "./components/Header";
+import Home from "./pages/home";
+import Footer from "./components/Footer";
+
 import "./styles/component/discutionForm.css";
 import "./styles/component/loginForm.css";
+import "./styles/component/messageForm.css";
+import "./styles/component/registerForm.css";
 
 import LoginForm from "./components/forms/LoginForm";
 import RegisterForm from "./components/forms/RegisterForm";
@@ -15,20 +17,25 @@ function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Route path="/" element={<Home />} />
-      <Route path="/discution/:id" element={<DiscutionPage />} />
-      <Route
-        path="/login"
-        element={<LoginForm onSubmit={(d) => console.log("login", d)} />}
-      />
-      <Route
-        path="/register"
-        element={<RegisterForm onSubmit={(d) => console.log("register", d)} />}
-      />
-      <Route
-        path="/discutionForm"
-        element={<DiscutionForm onSubmit={(d) => console.log("discution", d)} />}
-      />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/discution/:id" element={<DiscutionPage />} />
+
+        <Route
+          path="/login"
+          element={<LoginForm onSubmit={(d) => console.log("login", d)} />}
+        />
+        <Route
+          path="/register"
+          element={<RegisterForm onSubmit={(d) => console.log("register", d)} />}
+        />
+        <Route
+          path="/discutionForm"
+          element={<DiscutionForm onSubmit={(d) => console.log("discution", d)} />}
+        />
+      </Routes>
+
       <Footer />
     </BrowserRouter>
   );
--- front/src/components/DiscutionCards.jsx
+++ front/src/components/DiscutionCards.jsx
@@ -5,29 +5,29 @@ import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 
 export default function discutionCards() {
-    // const [discutions, setDiscutions] = useState([]);
-    const discutions= [ { id: 1, title: "Problème avec React", description: "Je n’arrive pas à comprendre pourquoi mon composant ne se rend pas correctement.", id_user: 1, last_modified: "2026-02-08T14:32:00" }, { id: 2, title: "CSS Flexbox ou Grid ?", description: "Dans quels cas vaut-il mieux utiliser Flexbox plutôt que Grid ?", id_user: 2, last_modified: "2026-02-07T18:10:00" }, { id: 3, title: "Vite ne démarre pas", description: "Erreur au lancement du serveur Vite après l’installation d’un package.", id_user: 3, last_modified: "2026-02-06T09:45:00" }, { id: 4, title: "Problème de sticky en CSS", description: "Mon aside en position sticky ne reste pas fixé au scroll.", id_user: 4, last_modified: "2026-02-05T21:02:00" } ];
-    const users = [
-      { id: 1, username: "Alice" },
-      { id: 2, username: "Bob" },
-      { id: 3, username: "Charlie" },
-      { id: 4, username: "Diane" }
-    ];
-    const post={
-       post_count:1
-    }
-//   useEffect(() => {
-// async function getDiscution(){
-//   try{
-//     const data= await api.get("/discussions")
-//     setDiscutions(data)
-    
-//   } catch(err){
-//     console.log(err)
-//   }
-// }
-// getDiscution()
-//   },[])
+  // const [discutions, setDiscutions] = useState([]);
+  const discutions = [{ id: 1, title: "Problème avec React", description: "Je n’arrive pas à comprendre pourquoi mon composant ne se rend pas correctement.", id_user: 1, last_modified: "2026-02-08T14:32:00" }, { id: 2, title: "CSS Flexbox ou Grid ?", description: "Dans quels cas vaut-il mieux utiliser Flexbox plutôt que Grid ?", id_user: 2, last_modified: "2026-02-07T18:10:00" }, { id: 3, title: "Vite ne démarre pas", description: "Erreur au lancement du serveur Vite après l’installation d’un package.", id_user: 3, last_modified: "2026-02-06T09:45:00" }, { id: 4, title: "Problème de sticky en CSS", description: "Mon aside en position sticky ne reste pas fixé au scroll.", id_user: 4, last_modified: "2026-02-05T21:02:00" }];
+  const users = [
+    { id: 1, username: "Alice" },
+    { id: 2, username: "Bob" },
+    { id: 3, username: "Charlie" },
+    { id: 4, username: "Diane" }
+  ];
+  const post = {
+    post_count: 1
+  }
+  //   useEffect(() => {
+  // async function getDiscution(){
+  //   try{
+  //     const data= await api.get("/discussions")
+  //     setDiscutions(data)
+
+  //   } catch(err){
+  //     console.log(err)
+  //   }
+  // }
+  // getDiscution()
+  //   },[])
 
   return (
     <div className="discutionContainer">
@@ -38,17 +38,17 @@ export default function discutionCards() {
 
         return (
           <div key={discussion.id} className="discutionCard">
-            <Link to={`/discussion/${discussion.id}`} key={discussion.id}  >
-            <h3>{discussion.title}</h3>
-            <p>{discussion.description}</p>
+            <Link to={`/discution/${discussion.id}`} key={discussion.id}  >
+              <h3>{discussion.title}</h3>
+              <p>{discussion.description}</p>
 
-            <div className="discutionLoop">
-              <span>{user?.username}</span>
-              <span>•</span>
-              <span>{discussion.last_modified}</span>
-              <span className="postCount"><FaMessage /> {post?.post_count}</span>
-            </div>
-          </Link>
+              <div className="discutionLoop">
+                <span>{user?.username}</span>
+                <span>•</span>
+                <span>{discussion.last_modified}</span>
+                <span className="postCount"><FaMessage /> {post?.post_count}</span>
+              </div>
+            </Link>
           </div>
         );
       })}
